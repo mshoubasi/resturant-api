b8cd5ec1f5f6e99ef87610516a0e4376870b7b76
mshoubasi <mhd.shoubasi@gmail.com>
2024-03-18 09:56:28 +0300

    created and added the relations between category and item

---
+++ app/Models/Item.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['category_id', 'name', 'price'];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
